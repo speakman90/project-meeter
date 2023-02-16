0905aee1397a5be463ab1c3ea809b6bf14e02196
Gaetan-Tcode <teixeira.gaetan@outlook.fr>
2023-02-16 17:17:48 +0100

    Creation de l'Auth. avec validation par mail

--- app/src/Entity/Users.php
+++ app/src/Entity/Users.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'Cette email n\'est pas disponible')]
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -72,8 +74,12 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $lastLocation = null;
 
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
+
     public function __construct()
     {
+        $this->createDate = new \DateTimeImmutable();
         $this->activities = new ArrayCollection();
         $this->messages = new ArrayCollection();
     }
@@ -354,6 +360,23 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLastLocation(?string $lastLocation): self
     {
         $this->lastLocation = $lastLocation;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function getIsVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }

93be453f3ca6ba9785bb58b7fd9484c2c86b7b49
speakman90 <97603002+speakman90@users.noreply.github.com>
2023-02-17 12:05:07 +0100

    Ajout tailwind

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -65,6 +65,9 @@ Encore
     // uncomment if you use React
     .enableReactPreset()
 
+    // uncomment if you use Tailwind
+    .enablePostCssLoader()
+
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
